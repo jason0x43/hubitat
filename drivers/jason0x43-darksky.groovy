c45a062ea5f4fe772105d643305877c26acd46f0
Jason Cheatham <j.cheatham@gmail.com>
2018-12-01 15:18:28 -0500

    WIP add a darksky driver

--- drivers/jason0x43-darksky.groovy
+++ drivers/jason0x43-darksky.groovy
@@ -4,11 +4,19 @@
 
 metadata {
     definition(name: 'DarkSky', namespace: 'jason0x43', author: 'Jason Cheatham') {
-        capability 'Notification'
-        capability 'Sensor'
-        capability 'Temperature Measurement'
-        capability 'Illuminance Measurement'
-        capability 'Relative Humidity Measurement'
+        capability('Sensor')
+        capability('Temperature Measurement')
+        capability('Illuminance Measurement')
+        capability('Relative Humidity Measurement')
+        capability('Refresh')
+
+        attribute('temperatureHi', 'number')
+        attribute('temperatureHiTime', 'number')
+        attribute('temperatureLo', 'number')
+        attribute('temperatureLoTime', 'number')
+        attribute('temperature', 'number')
+        attribute('precipChance', 'number')
+        attribute('cloudCover', 'number')
     }
 }
 
@@ -16,15 +24,14 @@ preferences {
     input(
         name: 'apiKey',
         type: 'text',
-        title: 'API Key:',
-        description: 'Dark Sky API Key ',
+        title: 'Dark Sky API Key:',
+        description: '',
         required: true
     )
     input(
         name: 'pollInterval',
         type: 'enum',
         title: 'Poll interval',
-        description: 'Poll interval in minutes',
         defaultValue: '30 Minutes',
         options: ['10 Minutes', '30 Minutes', '1 Hour']
     )
@@ -42,19 +49,39 @@ def init() {
     unschedule()
 
     if (settings.apiKey) {
-        def pollInterval = (settings.pollInterval ?: '30 minutes').replace(' ', '')
+        def pollInterval = (settings.pollInterval ?: '30 Minutes').replace(' ', '')
         "runEvery${pollInterval}"(poll)
+        log.debug "Scheduled poll for every ${pollInterval}"
+        // poll()
+        log.debug "${location.hubs[0]}"
     }
 }
 
 def poll() {
     try {
+        log.debug "Requesting data..."
+        def latitude = location.getLatitude()
+        def longitude = location.getLongitude()
         httpGet(
-            uri
+            "https://api.darksky.net/forecast/${settings.apiKey}/${latitude},${longitude}"
         ) { resp -> 
-            log.debug "response: ${resp}"
+            def data = resp.data
+            def now = data.currently;
+            sendEvent(name: 'temperature', value: now.temperature, unit: 'F')
+            sendEvent(name: 'precipChance', value: now.precipProbability, unit: '%')
+            sendEvent(name: 'cloudCover', value: now.cloudCover, unit: '%')
+
+            def today = data.daily.data[0];
+            sendEvent(name: 'temperatureHi', value: today.temperatureHigh, unit: 'F')
+            sendEvent(name: 'temperatureHiTime', value: today.temperatureHighTime, unit: 's')
+            sendEvent(name: 'temperatureLo', value: today.temperatureLow, unit: 'F')
+            sendEvent(name: 'temperatureLoTime', value: today.temperatureLowTime, unit: 's')
         }
     } catch (e) {
         log.error "Error updating: ${e}"
     }
+}
+
+def refresh() {
+    poll()
 }
